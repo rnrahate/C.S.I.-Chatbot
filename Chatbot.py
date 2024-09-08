436e022aa922ea48be040eae78317e3011209517
Aryan Rahate <rahatearyan.ar23062004@gmail.com>
2024-09-08 12:32:50 +0530

    fisrt commit

---
+++ Chatbot.py
@@ -0,0 +1,15 @@
+/* Pulse Animation */
+/* @keyframes pulse {
+  0% {
+    transform: translate(-50%, -50%) scale(1);
+    opacity: 1;
+  }
+  50% {
+    transform: translate(-50%, -50%) scale(1.1);
+    opacity: 0.7;
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(1);
+    opacity: 1;
+  }
+} */
